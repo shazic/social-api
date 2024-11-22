6324245ddcab85967e1b2527141ea38821ae4490
Shashank Chattopadhyaya <Schattopadhyaya@nextgen.com>
2024-11-22 13:40:52 +0530

    Split API into routers

--- api/main.py
+++ api/main.py
@@ -1,22 +1,6 @@
 from fastapi import FastAPI
-from models.post import UserPost, UserPostIn
+from routers.post import router as post_router
 
 app = FastAPI()
 
-
-post_table = {}
-
-
-@app.post("/post", response_model=UserPost)
-async def create_post(post: UserPostIn):
-    data = post.dict()
-    last_record_id = len(post_table)
-    new_post = {**data, "id": last_record_id}
-    post_table[last_record_id] = new_post
-    return new_post
-
-
-@app.get("/post", response_model=list[UserPost])
-async def get_posts():
-    return list(post_table.values())
-    return list(post_table.values())
+app.include_router(post_router)
