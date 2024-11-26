82db16b7412728e9d3330f70dca7039e15636bda
Shashank Chattopadhyaya <Schattopadhyaya@nextgen.com>
2024-11-26 14:46:39 +0530

    Add test to create a post

--- tests/routers/test_post.py
+++ tests/routers/test_post.py
@@ -10,3 +10,13 @@ async def create_post(body: str, async_client: AsyncClient) -> dict:
 @pytest.fixture()
 async def created_post(async_client: AsyncClient):
     return create_post("Hello world", async_client)
+
+
+@pytest.mark.anyio
+async def test_create_post(async_client: AsyncClient):
+    body = "Test Post"
+
+    response = await async_client.post("/post", json={"body": body})
+
+    assert response.status_code == 200
+    assert {"id": 0, "body": body}.items() <= response.json().items()
